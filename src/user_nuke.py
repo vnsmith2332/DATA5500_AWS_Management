ecf270ff5fda70fb6dfcb4145dfbb974e1c69cc3
EC2 Default User <ec2-user@ip-172-31-30-138.us-west-1.compute.internal>
2023-08-03 18:34:04 +0000

    changed nuke_environments() to handle 25 envs at a time, avoiding ValidationError

--- src/user_nuke.py
+++ src/user_nuke.py
@@ -9,25 +9,27 @@ def nuke_environments():
     """
     cloud9_client = boto3.client("cloud9")
     next_token = ""
-    environment_ids = []
-
-    # get all env ids
+    
     print("Deleting student Cloud9 Environments...")
+    
+    
     while True:
+        # get all environments, get environment details 25 at a time (defualt)
         list_env_response = cloud9_client.list_environments(nextToken=next_token)
-        environment_ids += list_env_response["environmentIds"]
+        environment_ids = list_env_response["environmentIds"]
+        environment_details = cloud9_client.describe_environments(environmentIds=environment_ids)
+        
+        # check ownerArn for each env, delete if it belongs to a student
+        for env in environment_details["environments"]:
+            if STUDENT_IAM_SUBSTRING in env["ownerArn"]:
+                cloud9_client.delete_environment(environmentId=env["id"])
+                
+        # check for more results waiting to be retrieved
         if "nextToken" not in list_env_response.keys():
             break
         next_token = list_env_response["nextToken"]
 
-    # describe all env, check ownerArn for student IAM user substring, delete env if True
-    if len(environment_ids) > 0:
-        environment_details = cloud9_client.describe_environments(environmentIds=environment_ids)
-        for env in environment_details["environments"]:
-            if STUDENT_IAM_SUBSTRING in env["ownerArn"]:
-                cloud9_client.delete_environment(environmentId=env["id"])
-    else:
-        print("No Cloud9 Environments to delete.")
+    print("Successfully deleted environments.\n")
 
 
 def nuke_users() -> None:
@@ -53,3 +55,9 @@ def nuke_users() -> None:
                 iam_client.delete_login_profile(UserName=user["UserName"])
                 iam_client.remove_user_from_group(GroupName=STUDENT_GROUP_NAME, UserName=user["UserName"])
                 iam_client.delete_user(UserName=user["UserName"])
+                
+    print("Successfully deleted users.\n")
+    
+if __name__ == "__main__":
+    nuke_environments()
+    nuke_users()
