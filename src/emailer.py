b9915a5cc392a64338b0863c01aa5a61ae26578d
EC2 Default User <ec2-user@ip-172-31-30-138.us-west-1.compute.internal>
2023-08-03 15:57:35 +0000

    minor changes to message string

--- src/emailer.py
+++ src/emailer.py
@@ -6,15 +6,16 @@ def send_credentials(user_details: dict):
     associated student's email
     """
     SENDER_EMAIL = "data5500.credentials@gmail.com"
-    AWS_ROOT_ACCT_ID = "410215147971"
+    AWS_ROOT_ACCT_ID = "141016442588"
 
     recipient_email = user_details["User"]["Email"]
     username = user_details["User"]["UserName"]
     password = user_details["User"]["Password"]
     fname = user_details["User"]["FirstName"]
+    section = user_details["User"]["Section"]
 
-    subject = "DATA 3500/5500 AWS Login Credentials"
-    body = f"Hi {fname.capitalize()},\n\nWelcome to DATA 3500/5500! We're excited to have you in the course and can't wait to get to know you better!\n\nThis course utilizes the cloud platform Amazon Web Services (AWS) and its IDE, Cloud9. In order to allow you to access this service, we have created an account for you. All the information you need to sign in can be found below:\n\nLogin Page: https://{AWS_ROOT_ACCT_ID}.signin.aws.amazon.com/console\nUsername: {username}\nPassword: {password}\n\nAfter you access your account, you will be prompted to reset your password; please choose a secure password and do not share it with others. Your professor will provide further instructions on how to setup your Cloud9 environment. If you have any trouble logging in, please reach out to the professor and/or TAs ASAP!\n\nPlease do not respond to this email or attempt to get assistance via this email address. The address is not monitored and all mail is deleted automatically.\n\nAgain, welcome to the course! Here's to a great semester!\n\nDATA 3500/5500 Team"
+    subject = f"{section} AWS Login Credentials"
+    body = f"Hi {fname.capitalize()},\n\nWelcome to {section}! We're excited to have you in the course and can't wait to get to know you better!\n\nThis course utilizes the cloud platform Amazon Web Services (AWS) and its IDE, Cloud9. In order to allow you to access this service, we have created an account for you. All the information you need to sign in can be found below:\n\nLogin Page: https://{AWS_ROOT_ACCT_ID}.signin.aws.amazon.com/console\nUsername: {username}\nPassword: {password}\n\nAfter you access your account, you will be prompted to reset your password; please choose a secure password and do not share it with others. Your professor will provide further instructions on how to setup your Cloud9 environment. If you have any trouble logging in, please reach out to the professor and/or TAs ASAP!\n\nPlease do not respond to this email or attempt to get assistance via this email address. The address is not monitored and all mail is deleted automatically.\n\nAgain, welcome to the course! Here's to a great semester!\n\nDATA 3500/5500 Team"
     ses_client = boto3.client("ses")
     ses_client.send_email(
         Source=SENDER_EMAIL,
@@ -44,7 +45,8 @@ if __name__ == "__main__":
                         {"Email": "vnsmith2332@gmail.com",
                          "UserName": "victor.smith",
                          "Password": "password123",
-                         "FirstName": "Victor"
+                         "FirstName": "Victor",
+                         "Section": "DATA-5500-IO1"
                          }
                     }
 
