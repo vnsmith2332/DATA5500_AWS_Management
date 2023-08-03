cb768c9e3cca674c46daeb3a551c8d4580f4d5bb
EC2 Default User <ec2-user@ip-172-31-30-138.us-west-1.compute.internal>
2023-08-03 21:48:54 +0000

    enahance error interface; add real student data file

--- src/usage.py
+++ src/usage.py
@@ -1,3 +1,14 @@
-def usage():
-    print("USAGE: ../main.py ../STUDENT_DATA_FILE.csv")
-    exit(0)
+def usage(error="", additive=""):
+    """
+    Unified interface for error messages
+    
+    :param error: the type of error the user made
+    :param additive: any information from the user's input that should be included in the error message
+    """
+    
+    if error == "":
+        print("USAGE: python src/main.py UTILITY [STUDENT_DATA_FILE.csv]\n\nCreate IAM users for students with information from a data file OR destroy IAM users and resources associated with students\n\t- UTILITY: functionality of the program you would like to use.\n\t\t- 'create': option to create IAM users. Requires the follow up argument STUDENT_DATA_FILE.csv when invoked.\n\t\t- 'destroy': option to destroy IAM users and their resources.\n\t- STUDENT_DATA_FILE.csv: a file containing the requisite information to create and share a student IAM user. Required if the 'create' option is invoked.\n")
+        
+    elif error == "invalid_option":
+        print(f"python src/main.py UTILITY [STUDENT_DATA_FILE.csv]\n\nERROR: {additive} is an invalid option for UTILITY. Use either 'create' or 'destroy'.\n")
+    
