68ca610de5ceeb194607f4fa96b34ad3fcffa291
EC2 Default User <ec2-user@ip-172-31-30-138.us-west-1.compute.internal>
2023-08-03 15:58:57 +0000

    added section to return dict, added passwrd substring, changed STUDENT_IAM_SUBSTRING

--- src/user_factory.py
+++ src/user_factory.py
@@ -3,15 +3,15 @@ import string
 import random
 
 # substring to identify student users and resources
-STUDENT_IAM_SUBSTRING = "DATA-500"
+STUDENT_IAM_SUBSTRING = "-DATA-500"
 # name of group to which all students are added
-STUDENT_GROUP_NAME = "students"
+STUDENT_GROUP_NAME = "DATA3500-5500-students"
 
 
-def user_factory(first_name: str, last_name: str, a_number: str, email: str) -> dict:
+def user_factory(first_name: str, last_name: str, a_number: str, email: str, section: str) -> dict:
     """
     Given a single student's information, create an IAM user for the student, create a login profile,
-    and add the user to the student policy group.
+    and add the user to the DATA3500-5500-students policy group.
 
     Returns JSON containing the created user's information, as stored in AWS.
 
@@ -19,6 +19,7 @@ def user_factory(first_name: str, last_name: str, a_number: str, email: str) -> 
     :param last_name: the student's last name. Used to create the username.
     :param a_number: the student's A-Number. Added to the user as a tag.
     :param email: the student's email. Added to the user as a tag.
+    :return user_details: returns dict containing information necessary to send credentials
     """
     iam_client = boto3.client("iam")
 
@@ -41,7 +42,7 @@ def user_factory(first_name: str, last_name: str, a_number: str, email: str) -> 
         user_creation_response = iam_client.create_user(UserName=first_name.lower() + "." + last_name.lower() + STUDENT_IAM_SUBSTRING + "-1", Tags=tags)
 
     # create login profile
-    password = (''.join(random.choices(string.ascii_lowercase + string.digits, k=10)))
+    password = "A@1f"+(''.join(random.choices(string.ascii_lowercase + string.digits, k=10)))
     iam_client.create_login_profile(UserName=user_creation_response["User"]["UserName"], Password=password, PasswordResetRequired=True)
 
     # add user to group
@@ -52,6 +53,7 @@ def user_factory(first_name: str, last_name: str, a_number: str, email: str) -> 
     user_details["User"]["Password"] = password
     user_details["User"]["FirstName"] = first_name
     user_details["User"]["Email"] = email
+    user_details["User"]["Section"] = section
 
     return user_details
 
@@ -60,4 +62,4 @@ if __name__ == "__main__":
     """
     creating a test student
     """
-    user_factory(first_name="test", last_name="student", a_number="a12345678", email="test.student@notreal.com")
+    print(user_factory(first_name="test", last_name="student", a_number="a12345678", email="test.student@notreal.com", section="DATA-5500-IO1"))
