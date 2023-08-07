d4d6b1945b4e726afcc308728d403845b2f3c812
EC2 Default User <ec2-user@ip-172-31-30-138.us-west-1.compute.internal>
2023-08-07 18:46:56 +0000

    update main.py to parse course roster downloaded from canvas

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import sys
+from time import sleep
+
 from emailer import *
 from user_factory import *
 from user_nuke import *
+from constants import MAX_SEND_RATE
 from usage import usage
 
 
@@ -9,28 +12,40 @@ def main():
     # check for required args at command line
     if len(sys.argv) < 2:
         usage()
-
-    create_or_destroy = 0
-    while create_or_destroy not in ["1", "2"]:
-        create_or_destroy = input("Would you like to...\n\t1) Create student IAM users\n\t2) Destroy all student IAM users and their resources\n")
-        if create_or_destroy not in ["1", "2"]:
-            print("Invalid selection.")
-
-    with open(sys.argv[1]) as f:
-        f.readline()
-        if create_or_destroy == "1":
-            for line in f:
-                line = line.strip().lower()
-                values = line.split(",")
-                user_info = user_factory(first_name=values[0], last_name=values[1], a_number=values[2], email=values[3])
-                send_credentials(user_details=user_info)
-            print("All users created successfully!")
-
-        else:
+        
+    # destroy users
+    elif sys.argv[1].lower().strip() == "destroy":
+        print("You are about to destroy all IAM users and resources in all DATA X500 courses. All data contained in these resources will be lost. This cannot be undone. Would you like to proceed? [y/n] > ", end="")
+        proceed = input()
+        if proceed.lower() == "y":
             nuke_environments()
             nuke_users()
             print("All student users and their resources have been deleted/terminated.")
-
+        else:
+            print("Process terminated. No resources were effected.")
+            exit()
+    
+    # create users      
+    elif sys.argv[1].lower() == "create":
+        if len(sys.argv) < 3:
+            usage(error="insufficient_args")
+        with open(sys.argv[2]) as f:
+            f.readline()
+            emails_sent = 0
+            for line in f:
+                # limit send rate to MAX_SEND_RATE emails per second
+                if emails_sent % MAX_SEND_RATE == 0:
+                    sleep(1)
+                line = line.strip().lower().replace(" ", "")
+                values = line.split(",")
+                user_info = user_factory(first_name=values[2], last_name=values[1], a_number=values[3], email=values[4], section=values[5])
+                send_credentials(user_details=user_info)
+                emails_sent += 1
+            print("All users created successfully!")
+            
+    else:
+        usage(error="invalid_option", additive=sys.argv[1])
+            
 
 if __name__ == "__main__":
     main()
